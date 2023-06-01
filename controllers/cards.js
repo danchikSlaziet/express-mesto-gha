83393690bbd87c68c9bcc6c5fde789d5e01704de
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-06-01 16:12:26 +0300

    обработка ошибок
    черновой вариант

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,19 +3,43 @@ const Card = require('../models/card');
 const getAllCards = (req, res) => {
   Card.find({})
     .then((card) => res.status(200).send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({
+      message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+    }));
 };
 const addNewCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные', name: err.name, error: err.message, stack: err.stack,
+        });
+        return;
+      }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        // Почему-то при несуществующем ID может возвращаться
+        // ответ null, поэтому пришлось сделать этот блок if/else
+        res.status(404).send({ message: 'Карточка с данным ID не найдена' });
+      }
+    })
+    .catch((err) => {
+      res.status(404).send({
+        message: 'Карточка с данным ID не найдена', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
@@ -23,8 +47,24 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Карточка с данным ID не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({
+          message: 'ID карточки не подходит под стандарт ObjectID', name: err.name, error: err.message, stack: err.stack,
+        });
+        return;
+      }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
@@ -32,8 +72,24 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Карточка с данным ID не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({
+          message: 'ID карточки не подходит под стандарт ObjectID', name: err.name, error: err.message, stack: err.stack,
+        });
+        return;
+      }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 module.exports = {
   getAllCards,
--- controllers/users.js
+++ controllers/users.js
@@ -3,24 +3,65 @@ const User = require('../models/user');
 const getAllUsers = (req, res) => {
   User.find({})
     .then((user) => res.status(200).send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({
+      message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+    }));
 };
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Пользователь по указанному ID не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({
+          message: 'Пользователь по указанному ID не найден, либо ID пользователя не подходит под стандарт ObjectID', name: err.name, error: err.message, stack: err.stack,
+        });
+        return;
+      }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const addNewUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные', name: err.name, error: err.message, stack: err.stack,
+        });
+        return;
+      }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: false })
+    .then((user) => {
+      // eslint-disable-next-line max-len
+      if ((user.name.length < 30 && user.name.length > 1) && (user.about.length < 30 && user.about.length > 1)) {
+        res.send({ data: user });
+      } else {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении профиля',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Ошибка по умлочанию', name: err.name, error: err.message, stack: err.stack,
+      });
+    });
 };
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
