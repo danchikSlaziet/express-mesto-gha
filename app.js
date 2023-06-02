4c5bb295ff85cf70eabb27d35c0b97fb2303b7fd
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2023-06-02 05:00:15 +0300

    правки на несуществующие роуты

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cardRouter = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const ERR404 = require('./utils/error-codes');
+const { ERR404 } = require('./utils/error-codes');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => console.log('MongoDB подключён'))
@@ -17,15 +17,14 @@ app.use((req, res, next) => {
   req.user = {
     _id: '64784f1ed9080c0d23d34c10',
   };
-
   next();
 });
+
 app.use('/users', userRouter);
 app.use('/', cardRouter);
-app.use('*', (req, res) => {
+app.use('/*', (req, res) => {
   res.status(ERR404).send({ message: 'Кривой маршрут, прочитайте документацию к API' });
 });
-
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
 });
